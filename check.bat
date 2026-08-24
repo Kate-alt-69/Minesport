96bc8a32197bba4bb6a9a03fb2916a2a7ef37e1e
Kate <dhwanksh2000@gmail.com>
2026-08-24 08:46:15 +0530

    Route Windows fast check through smart builder

--- check.bat
+++ check.bat
@@ -1,15 +1,5 @@
 @echo off
 setlocal EnableExtensions
 
-where pwsh >nul 2>nul
-if not errorlevel 1 goto use_pwsh
-
-goto use_windows_powershell
-
-:use_pwsh
-pwsh -NoProfile -ExecutionPolicy Bypass -File "%~dp0check.ps1" %*
-exit /b %errorlevel%
-
-:use_windows_powershell
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0check.ps1" %*
+call "%~dp0build.bat" --check %*
 exit /b %errorlevel%
